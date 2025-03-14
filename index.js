8de98c06fef4ae8cc6f5c6009f69d0b6961d2d01
manuel_gerardo <mgerardo17@alumnos.uaq.mx>
2025-03-13 23:12:01 -0600

    Añadiendo rutas POST

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
+const bodyparser=require('body-parser');
 const express = require("express");
 const req = require("express/lib/request");
 const app= express();
 const {pokemon} = require('./pokedex.json');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 /* 
 
 Verbos HTTP
 GET, Obtener un recurso
-POST, Guardar o Publicar algo
+POST, Guardar o Publicar algo, almacenar o crear
 PATCH, Actualización de un dato de un recurso en específico
-PUT, modifica todos los elementos
+PUT, modifica todos los elementos, un recurso completo
 DELETE, elimina un recurso
 */
 
 app.get("/", (req,res,next)=>{
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.get('/pokemon/all',(req,res,next)=>{
+app.post("/pokemon", (req,res, next) =>{
+    return res.status(200).send(req.body);
+});
+
+app.get('/pokemon',(req,res,next)=>{
     console.log(req.params.name);
-    res.status(200);
-    res.send(pokemon);
+    return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req,res,next)=>{
     const id=req.params.id - 1;
-    if(id >=0 && id <= 150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1]);
-    }else{
-        res.status(404);
-        res.send("Pokémon no encontrado");
+    if (id >=0 && id <= 150) { 
+        return res.status(200).send(pokemon[req.params.id - 1]) 
     }
+            
+    return res.status(404).send("Pokémon no encontrado");
+        
+    
     
 });
 
-app.get('/pokemon/:name',(req,res,next)=>{
+app.get('/pokemon/:name([A-Za-z]+)',(req,res,next)=>{
     const name=req.params.name;
-    for(i=0; i<pokemon.length;i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            return res.send(pokemon[i]);
+    /*for(i=0; i<pokemon.length;i++){
+        if(pokemon[i].name.toUpperCase() == name.toUpperCase()){
+            return res.status(200).send(pokemon[i]);
         }
+    }*/
+
+        //Operador Ternario, Concición ? retorno valor si verdadero : retorno valor si falso, se usa cuando queremos que de un If  se 
+        // retorne algo
+    const pk =pokemon.filter((p)=>{
+        return (p.name.toUpperCase()==name.toUpperCase()) && p ;
+
+    });
+
+    /*(pk.length >0) ?  
+    res.status(200).send(pk) : 
+    res.status(404).send("Pokémon no encontrado");*/
+
+    if(pk.length >0) { 
+        return res.status(200).send(pk); 
     }
-    res.status(404);
-    res.send("Pokémon no encontrado");
+    return res.status(404).send("Pokémon no encontrado");
+    
+    
 });
 
 app.listen(process.env.PORT||3000, ()=>{
